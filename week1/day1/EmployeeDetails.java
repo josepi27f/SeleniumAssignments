82a03c8de2ffceee1406ec32bfc63437d5b80beb
josepi27f <josepi27f@gmail.com>
2025-12-07 19:12:31 +1100

    Add files via upload
    week1

---
+++ week1/day1/EmployeeDetails.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "John Doe";
+		byte age = 30;
+		double salary = 55000.75;
+		char gender = 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber = 9876543210L;
+		int pinCode = 56001;
+		byte luckyNumber = 7;
+		System.out.println(name+"\n"+age+"\n"+
+				salary+"\n"+gender+"\n"+acceptTerms+
+				"\n"+rating+"\n"+mobileNumber+"\n"+
+				pinCode+"\n"+luckyNumber);
+
+	}
+
+}
---
+++ week1/day1/Firefox.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        double browserVersion=100.2;
+        String browserName="firefox";
+        boolean isVisible=true;
+        short releaseYear=1998;
+        char browserLogo='f';
+        System.out.println(browserVersion+browserName+isVisible+releaseYear+browserLogo);
+        
+	}
+
+}
